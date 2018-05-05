e9130975a16e23a803113726f805f8170e0e3fab
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-05-05 00:21:02 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,15 +1,15 @@
-# class Genre
-#   attr_accessor :name, :songs, :artist
-# 
-#   def initialize(name)
-#     @name = name
-#     @songs = []
-#   end
-# 
-#   # def artist
-#   #   @songs.collect do |song|
-#   #     song.artist 
-#   #   end 
-#   # end
-# 
-# end
+class Genre
+  attr_accessor :name, :songs, :artist
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  # def artist
+  #   @songs.collect do |song|
+  #     song.artist
+  #   end
+  # end
+
+end
